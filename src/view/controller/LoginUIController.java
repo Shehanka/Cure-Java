89185dde176e73acc959d40699919157b4ff02b9
Shehanka <hcsperera@outlook.com>
2018-09-08 15:08:38 +0530

    Initial commit

---
+++ src/view/controller/LoginUIController.java
@@ -0,0 +1,17 @@
+package view.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author chamodshehanka on 9/8/2018
+ * @project Cure-Java
+ **/
+public class LoginUIController implements Initializable{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
